c920cbf3b107b8f2f66fff3793eb5a2b8f6e3fed
細川浩太 <k.hosokawa0716@gmail.com>
2021-08-02 22:40:58 +0900

    first commit

---
+++ web/laravel/routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Http\Request;
+
+Route::post('/register', 'Auth\RegisterController@register')->name('register');
+Route::post('/login', 'Auth\LoginController@login')->name('login');
+Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
