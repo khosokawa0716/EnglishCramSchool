a5066c7d5fc47bcc238a5ae3aae597fc40a5eac5
細川浩太 <k.hosokawa0716@gmail.com>
2021-08-06 07:23:41 +0900

    add vuex

--- web/laravel/resources/js/app.js
+++ web/laravel/resources/js/app.js
@@ -1,6 +1,8 @@
+import './bootstrap'
 import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
+import store from './store'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
 Vue.use(Vuetify)
@@ -8,6 +10,7 @@ Vue.use(Vuetify)
 new Vue({
   el: '#app',
   router, // ルーティングの定義を読み込む
+  store,
   components: { App }, // ルートコンポーネントの使用を宣言する
   template: '<App />', // ルートコンポーネントを描画する
   vuetify: new Vuetify({
